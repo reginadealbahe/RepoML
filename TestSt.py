31f0ee7aea6d45157974c9d3206f6d1ce195c394
reginadealbahe <144020259+reginadealbahe@users.noreply.github.com>
2023-11-11 11:56:52 -0600

    Update TestSt.py

--- TestSt.py
+++ TestSt.py
@@ -7,6 +7,10 @@ features = ['EDUCATION', 'AGE','LIMIT_BAL','PAY_JUNE',
        'PAY_AMT_MAY', 'PAY_AMT_APRIL', 'PAY_AMT_MARCH', 'PAY_AMT_FEBRUARY', 'PAY_AMT_JANUARY','BILL_AMT_APRIL', 'BILL_AMT_FEBRUARY', 'BILL_AMT_JANUARY',
        'BILL_AMT_JUNE', 'BILL_AMT_MARCH', 'BILL_AMT_MAY']
 
+# Function to train model
+def train_model():
+       
+
 
 # Function to make predictions
 def make_prediction(input_data):
@@ -23,25 +27,39 @@ def make_prediction(input_data):
 def main():
     st.title("Prediction App")
 
-    # Upload a CSV file
-    st.write("Upload a CSV file for prediction.")
-    uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"])
+    # Read a default CSV file located in the same GitHub repository
+    default_file_path = 'train_customers.csv'  # Update with your actual file path
+    default_data = pd.read_csv(default_file_path)
 
-    if uploaded_file is not None:
-        # Read the uploaded CSV file into a Pandas DataFrame
-        input_data = pd.read_csv(uploaded_file)
+    # Display the default data
+    st.write("Default Data:")
+    st.write(default_data)
+       
+    while True:
+           # Upload a CSV file
+           st.write("Upload a CSV file for prediction.")
+           uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"])
+       
+           if uploaded_file is not None:
+               # Read the uploaded CSV file into a Pandas DataFrame
+               input_data = pd.read_csv(uploaded_file)
+       
+               # Display the uploaded data
+               #st.write("Uploaded Data:")
+               #st.write(input_data)
+       
+               # Make predictions
+               prediction = make_prediction(input_data)
+       
+               # Display the prediction
+               st.write("Prediction:", prediction)
 
-        # Display the uploaded data
-        st.write("Uploaded Data:")
-        st.write(input_data)
+           # Ask the user if they want to upload another CSV file
+            another_file = st.button("Upload Another CSV File")
 
-        # Make predictions
-        prediction = make_prediction(input_data)
-
-        # Display the prediction
-        st.write("Prediction:", prediction)
-
-   
+            if not another_file:
+                break  # Exit the loop if the user doesn't want to upload another file
+ 
 
 # Run the Streamlit app
 if __name__ == "__main__":
